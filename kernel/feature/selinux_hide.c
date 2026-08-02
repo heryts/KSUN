285fb9ef2044f9a589b382e7de15cbc43cd07202
heryts <heryts@gmail.com>
2026-08-02 17:34:41 +0700

    Update selinux_hide.c

--- kernel/feature/selinux_hide.c
+++ kernel/feature/selinux_hide.c
@@ -454,8 +454,8 @@ static void ksu_hide_sanitize_status(struct selinux_kernel_status *status)
     status->policyload = KSU_SELINUX_POLICYLOAD_SEQNO;
     status->sequence = 4;
 #else
-    status->policyload = 1;
-    status->sequence = 4;
+    status->policyload = 0;
+    status->sequence = 1;
 #endif
 
     if (ksu_late_loaded && !status->enforcing) {
