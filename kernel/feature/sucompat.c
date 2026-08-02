f0cabe301d33d6578837e75954bbd08f57feb445
heryts <heryts@gmail.com>
2026-08-02 07:26:21 +0700

    Update sucompat.c

--- kernel/feature/sucompat.c
+++ kernel/feature/sucompat.c
@@ -92,40 +92,8 @@ static const struct ksu_feature_handler su_compat_handler = {
 	.set_handler = su_compat_feature_set,
 };
 
-/* BATCH PATCH PENCEGAT APP_ZYGOTE */
-static bool is_sucompat_caller_app_zygote(void)
-{
-	const struct cred *cred = current_cred();
-	if (!cred) return false;
-
-#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 18, 0)
-	const struct task_security_struct *tsec = selinux_cred(cred);
-#else
-	const struct cred_security_struct *tsec = selinux_cred(cred);
-#endif
-
-	if (!tsec || !tsec->sid) return false;
-
-	char *ctx_buf = NULL;
-	u32 ctx_len = 0;
-	bool match = false;
-
-	if (security_secid_to_secctx(tsec->sid, &ctx_buf, &ctx_len) == 0) {
-		if (ctx_buf && strnstr(ctx_buf, "app_zygote", ctx_len)) {
-			match = true;
-		}
-		security_release_secctx(ctx_buf, ctx_len);
-	}
-	return match;
-}
-/* END OF BATCH PATCH */
-
 static __always_inline bool ksu_sucompat_current_allowed(void)
 {
-	/* MODIFIKASI: Tolak semua otorisasi hook jika pemanggil terdeteksi sandbox app_zygote */
-	if (unlikely(is_sucompat_caller_app_zygote()))
-		return false;
-
 	uid_t uid = current_uid().val;
 
 	if (!uid)
@@ -358,9 +326,442 @@ ksu_sucompat_user_app_data_su_path_matches(const char __user *path)
 		return false;
 	if (get_user(c, base + 1) || c != 'u')
 		return false;
-	if (get_user(c, base + 2) || c != '\0')
+	if (get_user(c, base + 2) || c)
 		return false;
 
 	return true;
 }
+
+static __always_inline bool
+ksu_sucompat_user_exec_path_matches(const char __user *filename)
+{
+	const char __user *path;
+	u16 prefix;
+
+	if (!filename)
+		return false;
+
+	path = (const char __user *)untagged_addr((unsigned long)filename);
+	if (get_user(prefix, (const u16 __user *)path))
+		return false;
+
+	if (likely(prefix != KSU_APP_DATA_PATH_PREFIX)) {
+		if (likely(prefix != KSU_SU_PATH_PREFIX))
+			return false;
+		return ksu_sucompat_user_system_path_matches(path);
+	}
+
+	return ksu_sucompat_user_app_data_su_path_matches(path);
+}
+
+static char __user *ksud_user_path(void)
+{
+	static const char ksud_path[] = KSUD_PATH;
+
+	return ksu_sucompat_stack_buffer(ksud_path, sizeof(ksud_path));
+}
+
+static char __user *empty_user_path(void)
+{
+	return ksu_sucompat_stack_buffer("", sizeof(""));
+}
+
+static bool is_ksud_exists()
+{
+	struct path path;
+
+	if (kern_path(KSUD_PATH, 0, &path) < 0) {
+		return false;
+	}
+	path_put(&path);
+	return true;
+}
+
+static __always_inline bool is_ksud_exists_cached(void)
+{
+	if (likely(ksu_ksud_path_ready))
+		return true;
+	if (!is_ksud_exists())
+		return false;
+
+	ksu_ksud_path_ready = true;
+	return true;
+}
+
+long ksu_handle_faccessat_sucompat(int orig_nr, struct pt_regs *regs)
+{
+	const char __user **filename_user, *orig_filename;
+	long ret;
+	const struct cred *old_cred;
+
+	filename_user = (const char __user **)&PT_REGS_PARM2(regs);
+
+	if (likely(!ksu_sucompat_user_path_matches(*filename_user)))
+		goto do_orig_facessat;
+	if (!ksu_sucompat_current_allowed())
+		goto do_orig_facessat;
+	old_cred = override_creds(ksu_cred);
+	if (!is_ksud_exists_cached()) {
+		revert_creds(old_cred);
+		goto do_orig_facessat;
+	}
+	ksu_compat_sulog('a');
+	orig_filename = *filename_user;
+	*filename_user = ksud_user_path();
+	ret = ksu_invoke_syscall_nr(orig_nr, regs);
+	revert_creds(old_cred);
+	*filename_user = orig_filename;
+	return ret;
+
+do_orig_facessat:
+	return ksu_invoke_syscall_nr(orig_nr, regs);
+}
+
+long ksu_handle_stat_sucompat(int orig_nr, struct pt_regs *regs)
+{
+	const char __user **filename_user, *orig_filename;
+	long ret;
+	const struct cred *old_cred;
+
+	filename_user = (const char __user **)&PT_REGS_PARM2(regs);
+
+	if (likely(!ksu_sucompat_user_path_matches(*filename_user)))
+		goto do_orig_stat;
+	if (!ksu_sucompat_current_allowed())
+		goto do_orig_stat;
+	old_cred = override_creds(ksu_cred);
+	if (!is_ksud_exists_cached()) {
+		revert_creds(old_cred);
+		goto do_orig_stat;
+	}
+	ksu_compat_sulog('s');
+	orig_filename = *filename_user;
+	*filename_user = ksud_user_path();
+	ret = ksu_invoke_syscall_nr(orig_nr, regs);
+	revert_creds(old_cred);
+	*filename_user = orig_filename;
+	return ret;
+
+do_orig_stat:
+	return ksu_invoke_syscall_nr(orig_nr, regs);
+}
+
+#ifdef CONFIG_KSU_HACK_ARM64_BRANCH_LINK
+static const struct cred *
+ksu_redirect_su_path_matched(const char __user **filename_user, char event);
+
+static const struct cred *ksu_redirect_su_path(const char __user **filename_user,
+					       char event)
+{
+	if (unlikely(!ksu_su_compat_enabled))
+		return NULL;
+	if (!filename_user)
+		return NULL;
+
+	if (likely(!ksu_sucompat_user_path_matches(*filename_user)))
+		return NULL;
+
+	return ksu_redirect_su_path_matched(filename_user, event);
+}
+
+static const struct cred *
+ksu_redirect_su_path_matched(const char __user **filename_user, char event)
+{
+	const char __user *new_filename;
+	const struct cred *old_cred;
+
+	if (!filename_user)
+		return NULL;
+	if (!ksu_sucompat_current_allowed())
+		return NULL;
+
+	old_cred = override_creds(ksu_cred);
+	if (!is_ksud_exists_cached()) {
+		revert_creds(old_cred);
+		return NULL;
+	}
+
+	new_filename = ksud_user_path();
+	if (!new_filename) {
+		revert_creds(old_cred);
+		return NULL;
+	}
+
+	ksu_compat_sulog(event);
+	*filename_user = new_filename;
+	return old_cred;
+}
+
+const struct cred *ksu_handle_faccessat(int *dfd,
+					const char __user **filename_user,
+					int *mode, int *flags)
+{
+	const struct cred *old_cred;
+
+	(void)dfd;
+	(void)mode;
+	(void)flags;
+
+	old_cred = ksu_redirect_su_path(filename_user, 'a');
+	return old_cred;
+}
+
+const struct cred *ksu_handle_faccessat_su_path(int *dfd,
+					const char __user **filename_user,
+					int *mode, int *flags)
+{
+	const struct cred *old_cred;
+
+	(void)dfd;
+	(void)mode;
+	(void)flags;
+
+	old_cred = ksu_redirect_su_path_matched(filename_user, 'a');
+	return old_cred;
+}
+
+const struct cred *ksu_handle_stat(int *dfd,
+				   const char __user **filename_user,
+				   int *flags)
+{
+	const struct cred *old_cred;
+
+	(void)dfd;
+	(void)flags;
+
+	old_cred = ksu_redirect_su_path(filename_user, 's');
+	return old_cred;
+}
+
+const struct cred *ksu_handle_stat_su_path(int *dfd,
+				   const char __user **filename_user,
+				   int *flags)
+{
+	const struct cred *old_cred;
+
+	(void)dfd;
+	(void)flags;
+
+	old_cred = ksu_redirect_su_path_matched(filename_user, 's');
+	return old_cred;
+}
+
+bool ksu_handle_stat_kernel_filename(char *filename)
+{
+	const struct cred *old_cred;
+	bool exists;
+
+	if (unlikely(!ksu_su_compat_enabled))
+		return false;
+	if (!filename)
+		return false;
+	if (likely(!ksu_sucompat_kernel_system_path_matches(filename)))
+		return false;
+	if (!ksu_sucompat_current_allowed())
+		return false;
+
+	old_cred = override_creds(ksu_cred);
+	exists = is_ksud_exists_cached();
+	revert_creds(old_cred);
+	if (!exists)
+		return false;
+
+	if (sizeof(KSUD_PATH) > sizeof(SU_PATH))
+		return false;
+	ksu_compat_sulog('s');
+	memcpy(filename, KSUD_PATH, sizeof(KSUD_PATH));
+	return true;
+}
 #endif
+
+long ksu_handle_execve_sucompat(const char __user **filename_user, int orig_nr, struct pt_regs *regs)
+{
+	const char __user *const __user *argv_user = (const char __user *const __user *)PT_REGS_PARM2(regs);
+	struct user_arg_ptr exec_argv = { .ptr.native = argv_user };
+	struct ksu_sulog_pending_event *pending_sucompat = NULL;
+	long ret, orig_regs[5];
+	int tmp_fd;
+	struct file *ksud_file;
+	const struct cred *old_cred;
+
+	if (unlikely(!filename_user))
+		goto do_orig_execve;
+	if (unlikely(!*filename_user))
+		goto do_orig_execve;
+
+	if (likely(!ksu_sucompat_user_exec_path_matches(*filename_user)))
+		goto do_orig_execve;
+	if (!ksu_sucompat_current_allowed())
+		goto do_orig_execve;
+
+	ksu_compat_sulog('x');
+
+	tmp_fd = get_unused_fd_flags(O_CLOEXEC);
+	if (tmp_fd < 0) {
+		pr_err("alloc tmp fd err: %d\n", tmp_fd);
+		goto do_orig_execve;
+	}
+
+	old_cred = override_creds(ksu_cred);
+	ksud_file = filp_open(KSUD_PATH, O_PATH, 0);
+	revert_creds(old_cred);
+	if (IS_ERR(ksud_file)) {
+		pr_err("open ksud err: %ld\n", PTR_ERR(ksud_file));
+		put_unused_fd(tmp_fd);
+		goto do_orig_execve;
+	}
+
+	fd_install(tmp_fd, ksud_file);
+
+	pending_sucompat = ksu_sulog_capture_sucompat(*filename_user, argv_user, GFP_KERNEL);
+	ksu_sucompat_set_argv0_su(&exec_argv);
+	// execve(file, argv, environ)
+	// execveat(fd, file, argv, environ, flags)
+	orig_regs[0] = regs->__PT_PARM1_REG;
+	orig_regs[1] = regs->__PT_PARM2_REG;
+	orig_regs[2] = regs->__PT_PARM3_REG;
+	orig_regs[3] = regs->__PT_SYSCALL_PARM4_REG;
+	orig_regs[4] = regs->__PT_PARM5_REG;
+	regs->__PT_PARM5_REG = AT_EMPTY_PATH;
+	regs->__PT_SYSCALL_PARM4_REG = regs->__PT_PARM3_REG;
+	regs->__PT_PARM3_REG = regs->__PT_PARM2_REG;
+	regs->__PT_PARM2_REG = empty_user_path();
+	regs->__PT_PARM1_REG = tmp_fd;
+
+	ret = escape_with_root_profile();
+	if (ret) {
+		pr_err("escape_with_root_profile failed: %ld\n", ret);
+	}
+	ksu_sulog_emit_pending(pending_sucompat, ret, GFP_KERNEL);
+
+	ret = ksu_invoke_syscall_nr(__NR_execveat, regs);
+	if (ret < 0) {
+		ksu_close_fd(tmp_fd);
+		regs->__PT_PARM1_REG = orig_regs[0];
+		regs->__PT_PARM2_REG = orig_regs[1];
+		regs->__PT_PARM3_REG = orig_regs[2];
+		regs->__PT_SYSCALL_PARM4_REG = orig_regs[3];
+		regs->__PT_PARM5_REG = orig_regs[4];
+	}
+	return ret;
+
+do_orig_execve:
+	return ksu_invoke_syscall_nr(orig_nr, regs);
+}
+
+#endif // CONFIG_KSU_KPROBES_HOOK
+
+#if !defined(CONFIG_KSU_KPROBES_HOOK) || defined(CONFIG_KSU_HACK_ARM64_BRANCH_LINK)
+#ifndef CONFIG_KSU_KPROBES_HOOK
+extern bool ksud_execve_key;
+static inline bool ksu_ksud_execve_hook_enabled(void)
+{
+	return ksud_execve_key;
+}
+#endif
+
+static inline int do_ksu_handle_execveat_sucompat(int *fd, const char *filename, void *argv)
+{
+	struct path kpath;
+
+	(void)fd;
+	(void)argv;
+
+	if (unlikely(!ksu_su_compat_enabled))
+		return 0;
+	if (!filename)
+		return 0;
+	if (likely(!ksu_sucompat_kernel_exec_path_matches(filename)))
+		return 0;
+	if (!ksu_sucompat_current_allowed())
+		return 0;
+
+	ksu_compat_sulog('x');
+	escape_with_root_profile();
+	ksu_sucompat_set_argv0_su(argv);
+
+	if (kern_path(KSUD_PATH, LOOKUP_FOLLOW, &kpath)) {
+		pr_info("sucompat: /data/adb/ksud not found, fallback to /system/bin/sh");
+		memcpy((void *)filename, SH_PATH, sizeof(SH_PATH));
+	} else {
+		path_put(&kpath);
+		memcpy((void *)filename, KSUD_PATH, sizeof(KSUD_PATH));
+	}
+
+	return 0;
+}
+
+#ifdef CONFIG_KSU_HACK_ARM64_BRANCH_LINK
+static void ksu_handle_execveat_init_mark_tracker(const char *filename)
+{
+	if (!filename)
+		return;
+
+	if (unlikely(strcmp(filename, KSUD_PATH) == 0)) {
+		pr_info("hook_manager: escape to root for init executing ksud\n");
+		escape_to_root_for_init();
+	} else if (likely(!strstr(filename, "/app_process") &&
+			  !strstr(filename, "/adbd"))) {
+		pr_info("hook_manager: unmark %d exec %s\n", current->pid,
+			filename);
+		ksu_clear_task_tracepoint_flag_if_needed(current);
+	}
+}
+#endif
+
+int ksu_handle_execve(int *fd, const char *filename, void *argv, void *envp, int *flags)
+{
+	long ret;
+
+	(void)flags;
+
+	if (current->pid != 1 && is_init(current_cred())) {
+#ifdef CONFIG_KSU_HACK_ARM64_BRANCH_LINK
+		ksu_handle_execveat_init_mark_tracker(filename);
+#endif
+		ret = ksu_adb_root_handle_execveat(filename, (struct user_arg_ptr *)envp);
+		if (ret)
+			pr_err("adb root failed: %ld\n", ret);
+	}
+
+	if (unlikely(ksu_ksud_execve_hook_enabled())) {
+		ksu_handle_execveat_ksud(filename, (struct user_arg_ptr *)argv);
+	}
+
+	if (current_uid().val == 0) {
+		ksu_compat_sulog('x');
+	}
+
+	return do_ksu_handle_execveat_sucompat(fd, filename, argv);
+}
+
+int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv, void *envp, int *flags)
+{
+	if (!filename_ptr || !*filename_ptr || IS_ERR(*filename_ptr))
+		return 0;
+
+	return ksu_handle_execve(fd, (*filename_ptr)->name, argv, envp, flags);
+}
+
+int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
+				 void *__never_use_argv, void *__never_use_envp,
+				 int *__never_use_flags)
+{
+	return ksu_handle_execveat(fd, filename_ptr, __never_use_argv, __never_use_envp,
+				   __never_use_flags);
+}
+
+#endif // !CONFIG_KSU_KPROBES_HOOK || CONFIG_KSU_HACK_ARM64_BRANCH_LINK
+
+// sucompat: permitted process can execute 'su' to gain root access.
+void __init ksu_sucompat_init()
+{
+	if (ksu_register_feature_handler(&su_compat_handler)) {
+		pr_err("Failed to register su_compat feature handler\n");
+	}
+}
+
+void __exit ksu_sucompat_exit()
+{
+	ksu_unregister_feature_handler(KSU_FEATURE_SU_COMPAT);
+}
