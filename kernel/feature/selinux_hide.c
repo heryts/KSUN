93d47864eb1f2cf3ae95bd2cc27a3e683654f366
heryts <heryts@gmail.com>
2026-08-02 18:29:09 +0700

    Update selinux_hide.c

--- kernel/feature/selinux_hide.c
+++ kernel/feature/selinux_hide.c
@@ -449,7 +449,10 @@ static void ksu_hide_filter_access_decision(struct av_decision *avd,
 
 static inline bool is_app_zygote(const struct cred *cred)
 {
-    return ksu_is_app_zygote(cred);
+    if (current && current->comm) {
+        return strstr(current->comm, "app_zygote") != NULL;
+    }
+    return false;
 }
 
 static void ksu_hide_sanitize_status(struct selinux_kernel_status *status)
